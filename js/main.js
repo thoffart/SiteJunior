5c64a52674da50f71ce40bcde5aee368ef1ecaff
denisakira <denis_akira@hotmail.com>
2017-10-16 22:32:46 -0200

    1.1

--- js/main.js
+++ js/main.js
@@ -47,6 +47,70 @@ $(document).ready(function(){
         }
     );
 
+    /*$('input, textarea').data('holder', $('input, textarea').attr('placeholder'));
+
+    $('input, textarea').focusin(function () {
+        $(this).attr('placeholder', '');
+    });
+    $('input, textarea').focusout(function () {
+        $(this).attr('placeholder', $(this).data('holder'));
+    });*/
+
+
+    //contact form validation
+    $("#contact-form").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            message: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Please enter Your Name",
+                minlength: "Your name must consist of at least 2 characters"
+            },
+            message: {
+                required: "Please Write Something",
+                minlength: "Your message must consist of at least 2 characters"
+            },
+            email: "Please enter a valid email address"
+        },
+        submitHandler: function(form) {
+            $(form).ajaxSubmit({
+                type:"POST",
+                data: $(form).serialize(),
+                url:"mail.php",
+                success: function() {
+                    $('#contact-form :input').attr('disabled', 'disabled');
+                    $('#contact-form').fadeTo( "slow", 0.15, function() {
+                        $(this).find(':input').attr('disabled', 'disabled');
+                        $(this).find('label').css('cursor','default');
+                        $('#success').fadeIn();
+                    });
+                },
+                error: function() {
+                    $('#contact-form').fadeTo( "slow", 0.15, function() {
+                        $('#error').fadeIn();
+                    });
+                }
+            });
+        }
+    });
 
 });
+
+
+
+
+
+
 
